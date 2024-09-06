fdd2e4a1813d5164cedab64cd090c978f9b9632c
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-09-06 20:04:45 -0300

    caches

--- src/app/api/verpedidos/route.js
+++ src/app/api/verpedidos/route.js
@@ -4,6 +4,11 @@ export async function GET(req) {
     const [row] = await db.query('SELECT * FROM pedido')
 
     return new Response(JSON.stringify(row), {
-        headers: {'Content-Type': 'application/json'}
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+            'Expires': '0',
+            'Pragma': 'no-cache'
+        }
     })
 }
